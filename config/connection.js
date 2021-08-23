a99e417e6ea19304a6c3467258e5b54ef3e499ec
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-08-23 15:54:49 +1000

    Initiate database connection

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mysql2 = require("mysql2");
+
+const db = mysql2.createConnection({
+    host: 'localhost',
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+},
+
+    console.log(`Connected to the ${database} database. `));
+
+db.connect((err) => {
+    if (err) throw err;
+});
+
+module.exports = db;
