db84676e96d335cc262a7088a90f55b02b84444f
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-08-25 16:43:51 +1000

    Changes to connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,14 @@ require("dotenv").config();
 const mysql2 = require("mysql2");
 
 const db = mysql2.createConnection({
-    host: 'localhost',
-    database: process.env.DB_NAME,
+    host: "localhost",
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
-},
-
-    console.log(`Connected to the database. `));
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
 
 db.connect((err) => {
+    console.log(`Connected to the database. `)
     if (err) throw err;
 });
 
